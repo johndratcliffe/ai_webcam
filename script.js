f33bb45f8bd3fd0bf0eb9d668b1eaf31aaff6d8c
John Ratcliffe <2095497@students.wits.ac.za>
2024-06-09 17:33:24 +0200

    COCO-SSD pre-trained model to detect objects in the webcam

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const video = document.getElementById('webcam')
+const liveView = document.getElementById('liveView')
 const demosSection = document.getElementById('demos')
 const enableWebcamButton = document.getElementById('webcamButton')
 
@@ -38,10 +39,53 @@ function enableCam (event) {
   })
 }
 
-// Placeholder function
-function predictWebcam () {
-}
+let model
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel
+  // Show demo section now that model is ready to use.
+  demosSection.classList.remove('invisible')
+})
 
-// Pretend model has loaded to see if webcam will load
-const model = true
-demosSection.classList.remove('invisible')
+const children = []
+function predictWebcam () {
+  // Start classifying a frame in the stream.
+  model.detect(video).then(function (predictions) {
+    // Remove any highlighting we did previous frame.
+    for (let i = 0; i < children.length; i++) {
+      liveView.removeChild(children[i])
+    }
+    children.splice(0)
+
+    // Loop through predictions and draw them to the live view if
+    // they have a high confidence score.
+    for (let n = 0; n < predictions.length; n++) {
+      // If we are over 66% sure we are sure we classified it right, draw it!
+      if (predictions[n].score > 0.66) {
+        const p = document.createElement('p')
+        p.innerText = predictions[n].class + ' - with ' +
+            Math.round(parseFloat(predictions[n].score) * 100) +
+            '% confidence.'
+        // Coordinates of the text
+        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: ' +
+            (predictions[n].bbox[1] - 10) + 'px; width: ' +
+            (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;'
+
+        // Coordinates and dimensions of the highlighted region
+        const highlighter = document.createElement('div')
+        highlighter.setAttribute('class', 'highlighter')
+        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: ' +
+            predictions[n].bbox[1] + 'px; width: ' +
+            predictions[n].bbox[2] + 'px; height: ' +
+            predictions[n].bbox[3] + 'px;'
+
+        liveView.appendChild(highlighter)
+        liveView.appendChild(p)
+        children.push(highlighter)
+        children.push(p)
+      }
+    }
+
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam)
+  })
+}
